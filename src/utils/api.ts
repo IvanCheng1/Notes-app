dd1c388768e60ce11ea7ca39ba3c4bf82a722852
Ivan Cheng <ivan@ivancheng.co.uk>
2020-09-09 20:30:30 +0100

    api refactor

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,27 +5,33 @@ const NOTES_STORAGE_KEY = "NOTES_STORAGE_KEY";
 export const getNotes = async (): Promise<string[] | []> => {
   // localStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(["Welcome note"]))
 
-  const data = localStorage.getItem(NOTES_STORAGE_KEY);
+  // const data = localStorage.getItem(NOTES_STORAGE_KEY);
 
-  // if (typeof data === "string") {
-  //   return JSON.parse(data)
-  // }
-
-  return JSON.parse(data || "[]");
+  return getLocalStorage();
 };
 
 export const addNote = async (note: string) => {
-  const prevNotes = JSON.parse(localStorage.getItem(NOTES_STORAGE_KEY) || "[]");
-  const notes = [...prevNotes, note];
-
-  localStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(notes));
+  // const prevNotes = JSON.parse(localStorage.getItem(NOTES_STORAGE_KEY) || "[]");
+  const notes = [...getLocalStorage(), note];
+  
+  // localStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(notes));
+  setLocalStorage(notes)
 };
 
 export const deleteNote = async (note: string) => {
-  const prevNotes = JSON.parse(localStorage.getItem(NOTES_STORAGE_KEY) || "[]");
-  const notes = prevNotes.filter((n: string) => n !== note);
-  
-  localStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(notes));
+  // const prevNotes = JSON.parse(localStorage.getItem(NOTES_STORAGE_KEY) || "[]");
+  const notes = getLocalStorage().filter((n: string) => n !== note);
+
+  // localStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(notes));
+  setLocalStorage(notes)
 };
+
+const getLocalStorage = (): string[] => {
+  return JSON.parse(localStorage.getItem(NOTES_STORAGE_KEY) || "[]");
+}
+
+const setLocalStorage = (notes: string[]): void => {
+  localStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(notes));
+}
 
 export {}; // to avoid TS error
